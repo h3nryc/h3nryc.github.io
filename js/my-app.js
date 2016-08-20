f353d418da8060b44049c701454c8834ddf50b14
h3nryc <henry.confos@student.tsc.nsw.edu.au>
2016-08-20 16:41:54 +1000

    Added nearby

--- js/my-app.js
+++ js/my-app.js
@@ -1,17 +1,133 @@
 var myApp = new Framework7(); 
- 
+var $$ = Dom7;
   // Init slider and store its instance in mySwiper variable
   var mySwiper = myApp.swiper('.swiper-container', {
     pagination:'.swiper-pagination'
   });
 
 
-
 function more() {
-	if ($(".wet-card").height() == 400) {
+	if ($(".wet-card").height() == 350) {
 			$(".wet-card").animate({height: 60});
+			$(".wet-hidden").toggle();
 	}else{
-		$(".wet-card").animate({height: 400});
+		$(".wet-card").animate({height: 350});
+		$(".wet-hidden").fadeToggle("slow")
 	}
 
 }
+var ptrContent = $$('.pull-to-refresh-content');
+ 
+// Add 'refresh' listener on it
+ptrContent.on('refresh', function (e) {
+		location.reload();
+        myApp.pullToRefreshDone();
+});
+
+//Nearby Places (This is front end for now but will be pushed to back)
+
+
+var clientSecret = "AV3AU1AIPBEZZMCTRZUWSVUZUZOOK00MGGI3NBHO04A4FTHH&v=20130815";
+var clientId = "YBQLVNSQZGPKCTLBYBZKTAJENWY1CVAOHBPMVNDGIFP1VE4Y";
+
+
+function getNearby(position) {
+	//Get geo location
+	if(localStorage.getItem("lat") == undefined){
+		navigator.geolocation.getCurrentPosition(function(location) {
+
+
+		  var lat = Number(location.coords.latitude).toFixed(2); 
+		  var long =  Number(location.coords.longitude).toFixed(2); 
+		  localStorage.setItem("lat", lat);
+		  localStorage.setItem("long", long);
+		  getNearby();
+	});
+	}else{
+		var apiLink = "https://api.foursquare.com/v2/venues/search?ll="+localStorage.getItem("lat")+","+localStorage.getItem("long")+"&client_id="+clientId+"&client_secret="+clientSecret+""
+		$.ajax({
+  		url: apiLink,
+  		dataType: 'jsonp',
+  	success: function(data){
+
+
+    	
+  			for (var i = 0; i < 5; i++) {
+           		console.log(data.response.venues[i])
+           		var name = data.response.venues[i].name;
+           		var type = data.response.venues[i].categories[0].shortName;
+           		var tip = data.response.venues[i].stats.tipCount;
+           		var address = data.response.venues[i].location.address;
+           		var city = data.response.venues[i].location.city;
+           		var vLat = data.response.venues[i].location.lat;
+           		var vLong = data.response.venues[i].location.long;
+           		var verified = data.response.venues[i].verified
+           		var id = data.response.venues[i].id
+           		$( ".wet-card" ).after( ' <li style="background-color: '+type2Color(type)+';"class="food-card"> <div class="food-head"> <h2>'+type2Emoji(type)+'  '+type+' - '+tip+' tips</h2> </div> <div class="food-hero"></div> <div class="food-footer"> <h2>'+name+'</h2> <p style="margin: 0;">This '+type+' is located on '+address+' '+city+'</p> </div> </li>' );
+           	}  
+
+  		}
+	});
+	}
+	  
+}
+
+
+
+
+
+function type2Emoji(type) {
+	if(type == "Beach"){
+		return "🏖";
+	}else if (type == "Park"){
+		return "🌲";
+	}else if (type == "Playground"){
+		return "⛳️";
+	}else if (type == "Food & Drink"){
+		return "🍴";
+	}else if(type == "Preserve"){
+		return "🍀";
+	}else if(type == "Historic Site"){
+		return "👴";
+	}else if (type == "Pool"){
+		return "🌊";
+	}else if (type == "Café"){
+		return "☕️";
+	}
+	else{
+		return "🚶";
+	}
+}
+
+
+function type2Color(type) {
+	if(type == "Beach"){
+		return "#f1c40f";
+	}else if (type == "Park"){
+		return "#2ecc71";
+	}else if (type == "Playground"){
+		return "#e74c3c";
+	}else if (type == "Food & Drink"){
+		return "#e67e22";
+	}else if(type == "Preserve"){
+		return "#EF2D56";
+	}else if (type == "Historic Site" || type == "Café"){
+		return '#ED7D3A';
+	}else if (type == "Pool"){
+		return "#5BC0EB";
+	}
+	else{
+		var rand = Math.floor(Math.random() * 3) + 1  
+		if (rand == 1) {
+			return "#03FCBA";
+		} else if (rand == 2) {
+			return "#01FDF6";
+		}else if (rand == 3){
+			return "#CBBAED";
+		}
+		
+	}
+}
+
+
+
--- js/weather.js
+++ js/weather.js
@@ -1,8 +1,47 @@
+  function code2Emoji(code) {
+    console.log(code)
+    if (code == '32') {
+            //Sunny
+           return '☀️';
+        } else if (code == '31') {
+            //Clear (Night)
+            return '🌃';
+        } else if (code == '26' || code == '34') {
+            //Cloudy
+            return '☁️';
+        } else if (code == '11' || code == '12' || code == '10') {
+            //Rain
+           return '🌧';
+        }else if (code == '30'){
+            //Partly Couldy 
+            return '⛅️';
+        }else if (code == '40'){
+            //Scattered showers
+            return '🌦';
+        }else if (code == '39'){
+            //scattered thunderstorms
+            return '⛈';
+        }
+        else{
+            //None
+            return '🌂';
+        }
+  }
   var callbackFunction1 = function(data) {
     var oneword = data.query.results.channel.item.condition.text;
+    var weathercode = data.query.results.channel.item.condition.code;
     var temp = Math.round((data.query.results.channel.item.condition.temp - 32) * 5 / 9);
     window.onload = function () {
     	document.getElementById('onewordweather').innerHTML = oneword;
+        document.getElementById('emoji-left').innerHTML = '<h2>'+code2Emoji(weathercode)+'</h2>';
     	document.getElementById('temp').innerHTML = temp + ' °C';
+
+        //Hidden div
+        
+        var forecast = data.query.results.channel.item.forecast;
+        for (var i = 0; i < 5; i++) {
+            var high = Math.round((forecast[i].high - 32) * 5 / 9);
+            $("#wet-list").append("<li><h2 style='font-weight: 200; margin: 0;' ><span style='margin: 0; font-size: 25pt;'>"+code2Emoji(forecast[i].code)+"</span> "+forecast[i].date+", "+forecast[i].text+"</h2></li>");
+        }    
     }
   };
