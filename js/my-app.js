ebe54d8720d266d59c0172ce4a308a620e18a06f
h3nryc <henry.confos@student.tsc.nsw.edu.au>
2016-08-24 12:04:35 +1000

    Updated to version 0.2

--- js/my-app.js
+++ js/my-app.js
@@ -1,3 +1,11 @@
+/*
+
+	  Unum (c) All rights reserved.
+	 The code that powers unum front.
+	Coded by Henry Confos & Tom Lister.
+
+*/
+
 var myApp = new Framework7(); 
 var $$ = Dom7;
   // Init slider and store its instance in mySwiper variable
@@ -24,6 +32,63 @@ ptrContent.on('refresh', function (e) {
         myApp.pullToRefreshDone();
 });
 
+/*
+
+Start Main Functions
+
+*/
+
+
+function getDistance(lat1,lon1,lat2,lon2) {
+  var R = 6371; // Radius of the earth in km
+  var dLat = deg2rad(lat2-lat1);  // deg2rad below
+  var dLon = deg2rad(lon2-lon1); 
+  var a = 
+    Math.sin(dLat/2) * Math.sin(dLat/2) +
+    Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * 
+    Math.sin(dLon/2) * Math.sin(dLon/2)
+    ; 
+  var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
+  var d = R * c; // Distance in km
+  return Number(d).toFixed(2);
+}
+
+function deg2rad(deg) {
+  return deg * (Math.PI/180)
+}
+
+function loadMore(type,lat,long,id,provider,name,address,emoji,color) {
+	console.log(1)
+	var distance = getDistance(lat,long,localStorage.getItem("lat"),localStorage.getItem("long"));
+	console.log(distance)
+	var time = distance * 0.50;
+	var uberFare = Number(distance * 1.50 + 5 + time).toFixed(2);
+	var taxiFare = Number(distance * 2.86 + 5).toFixed(2);
+	var uberLink = "https://m.uber.com/ul?client_id=YOUR_CLIENT_ID&action=setPickup&pickup[latitude]="+localStorage.getItem("lat")+"&pickup[longitude]=-"+localStorage.getItem("long")+"&pickup[nickname]=Your Location[formatted_address]=Your Location Rd&dropoff[latitude]="+lat+"&dropoff[longitude]=-"+long+"&dropoff[nickname]="+name+"[formatted_address]="+address+"&product_id=a1111c8c-c720-46c3-8534-2fcdd730040d&link_text=View%20team%20roster&partner_deeplink=Unum"
+	var taxiLink = "https://www.google.com.au/#safe=active&q=book%20a%20taxi"
+	var mapUrl = "http://maps.googleapis.com/maps/api/staticmap?center="+lat+","+long+"&zoom=18&size=500x400&sensor=false"
+	$('.main-list').after(' <div style="background-color: '+color+'" class="info-card fullscreen"> <div onclick="closePopup();" class="info-head"> <i class="material-icons">arrow_back</i><h2 class="info-title">'+emoji+'   '+name+'</h2> </div> <div class="info-map" style="background-image: url('+mapUrl+');"></div> <div class="info-footer"> <p style="margin: 0;">'+name+' is located on <span style="font-weight: bold;">'+address+'</span></p> <h2 style="margin: 0;">In a taxi it would cost around <span style="font-weight: bold;">'+taxiFare+'</span> or an Uber would cost <span style="font-weight: bold;">'+uberFare+'</span> 🚕</h2> <div class="info-butt"> <div class="info-book">Book a</div><a href="'+uberLink+'"><div class="uber">Uber</div></a><a href="'+taxiLink+'>"<div class="taxi">Taxi</div></a> </div> <p>Infomation provided by '+provider+' ✌️</p> </div> </div>');
+	$('.info-card').hide()
+	$('.info-card').animate({width: 'toggle'}, 120);
+}
+
+function closePopup() {
+	$('.info-card').animate({width: 'toggle'}, 120, function(){
+		$('.info-card').remove();
+	});
+}
+/*
+
+End Main Functions
+
+*/
+
+/*
+
+Start Nearby Places
+
+*/
+
 //Nearby Places (This is front end for now but will be pushed to back)
 
 
@@ -41,9 +106,27 @@ function getNearby(position) {
 		  var long =  Number(location.coords.longitude).toFixed(2); 
 		  localStorage.setItem("lat", lat);
 		  localStorage.setItem("long", long);
-		  getNearby();
+		  localStorage.setItem("quadTime", Date.now());
+		  getVenue();
+		  getEvent()
 	});
 	}else{
+		getVenue();
+		getEvent()
+	}
+	  
+}
+
+
+function getVenue() {
+	// Check if Quad is old
+	// if (Date.now() - localStorage.getItem("quadTime") >= 3600) {
+	// 	localStorage.removeItem("lat");
+	// 	 localStorage.removeItem("long");
+	// 	getNearby();
+	// }
+
+	//Get api request
 		var apiLink = "https://api.foursquare.com/v2/venues/search?ll="+localStorage.getItem("lat")+","+localStorage.getItem("long")+"&client_id="+clientId+"&client_secret="+clientSecret+""
 		$.ajax({
   		url: apiLink,
@@ -52,28 +135,26 @@ function getNearby(position) {
 
 
     	
-  			for (var i = 0; i < 5; i++) {
-           		console.log(data.response.venues[i])
+  			for (var i = 0; i < 6; i++) {
+  				//Unpack function
            		var name = data.response.venues[i].name;
            		var type = data.response.venues[i].categories[0].shortName;
            		var tip = data.response.venues[i].stats.tipCount;
            		var address = data.response.venues[i].location.address;
            		var city = data.response.venues[i].location.city;
            		var vLat = data.response.venues[i].location.lat;
-           		var vLong = data.response.venues[i].location.long;
+           		console.log(vLat)
+           		var vLong = data.response.venues[i].location.lng;
            		var verified = data.response.venues[i].verified
            		var id = data.response.venues[i].id
-           		$( ".wet-card" ).after( ' <li style="background-color: '+type2Color(type)+';"class="food-card"> <div class="food-head"> <h2>'+type2Emoji(type)+'  '+type+' - '+tip+' tips</h2> </div> <div class="food-hero"></div> <div class="food-footer"> <h2>'+name+'</h2> <p style="margin: 0;">This '+type+' is located on '+address+' '+city+'</p> </div> </li>' );
+           		var provider = "Foursquare"
+           		//Append data to list
+           		$( ".wet-card" ).slideDown().after( ' <li onclick="loadMore('+"'"+type+"'"+','+vLat+','+vLong+','+"'"+id+"'"+','+"'"+provider+"'"+','+"'"+name+"'"+','+"'"+address+"'"+','+"'"+type2Emoji(type)+"'"+','+"'"+type2Emoji(type)+"'"+')" style="background-color: '+type2Color(type)+';"class="food-card"> <div class="food-head"> <h2>'+type2Emoji(type)+'  '+type+' - '+tip+' tips</h2> </div> <div class="food-hero"></div> <div class="food-footer"> <h2>'+name+'</h2> <p style="margin: 0;">This '+type+' is located on '+address+' '+city+'</p> </div> </li>' );
            	}  
 
   		}
 	});
-	}
-	  
 }
-
-
-
 
 
 function type2Emoji(type) {
@@ -101,19 +182,19 @@ function type2Emoji(type) {
 
 
 function type2Color(type) {
-	if(type == "Beach"){
+	if(type == "Beach" || type == 1){
 		return "#f1c40f";
-	}else if (type == "Park"){
+	}else if (type == "Park" || type == 2){
 		return "#2ecc71";
-	}else if (type == "Playground"){
+	}else if (type == "Playground" || type == 3){
 		return "#e74c3c";
-	}else if (type == "Food & Drink"){
+	}else if (type == "Food & Drink" || type == 4){
 		return "#e67e22";
 	}else if(type == "Preserve"){
 		return "#EF2D56";
-	}else if (type == "Historic Site" || type == "Café"){
+	}else if (type == "Historic Site" || type == "Café" || type == 5){
 		return '#ED7D3A';
-	}else if (type == "Pool"){
+	}else if (type == "Pool" || type == 6){
 		return "#5BC0EB";
 	}
 	else{
@@ -128,7 +209,77 @@ function type2Color(type) {
 		
 	}
 }
+
+function getImage(query) {
+
+
+}
+
+//Call Function on load
 window.onload = function () {
  getNearby();   
 }
+
+/*
+
+End Nearby Places
+
+*/
+/*
+
+Start Events
+
+*/
+
+function getEvent() {
+	var eventKey = "HJRp25zS5jm5NJQr"
+	var eventLink = "http://api.eventful.com/json/events/search?app_key="+eventKey+"&where="+localStorage.getItem("lat")+","+localStorage.getItem("long")+"&within=25&units=km&sort_order=popularity"
+		$.ajax({
+  		url: eventLink,
+  		dataType: 'jsonp',
+  	success: function(data){
+  		
+
+
+  			for (var i = 0; i < 6; i++) {
+  				//Unpack function
+           		var name = data.events.event[i].title;
+           		var lat = data.events.event[i].latitude;
+           		var long = data.events.event[i].longitude;		
+				try {
+    				var image = data.events.event[i].image.medium.url;
+				}
+				catch(err) {
+    				var image = "https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcRzbjllLfcybGqvOmehP2qaxFPAs5IXz5XLolnAfu_CuXh5eFLoevIxsTuh"
+				}	
+           		var venueName = data.events.event[i].venue_name;
+           		var venueAddress = data.events.event[i].venue_address;
+           		var startTime = data.events.event[i].start_time;
+           		var id = data.events.event[i].id;
+           		var eventfulUrl = data.events.event[i].url;
+           		var cityName = data.events.event[i].city_name;
+           		//Append data to list
+           		var rand = Math.floor(Math.random() * 7) + 1
+           		$('#event-start').after(' <li style="background-color: '+type2Color(rand)+';" class="event-card"> <div class="event-head"> <h2>🎉 Event - '+name+'</h2> </div> <div style="background-image: url('+image+');" class="event-hero"></div> <div class="event-footer"> <h2>When - '+startTime+'</h2> <p>'+venueName+' / '+venueAddress+'</p> </div> </li>')
+           	} 
+
+
+
+
+  	}
+  })
+}
+
+
+/*
+
+End Events
+
+*/
+
+
+
+
+
+
 
--- js/weather.js
+++ js/weather.js
@@ -1,5 +1,11 @@
+  /*
+
+      Unum (c) All rights reserved.
+     The code that powers unum front.
+    Coded by Henry Confos & Tom Lister.
+
+*/
   function code2Emoji(code) {
-    console.log(code)
     if (code == '32') {
             //Sunny
            return '☀️';
@@ -21,6 +27,9 @@
         }else if (code == '39'){
             //scattered thunderstorms
             return '⛈';
+        }else if (code == '28'){
+            //Mostly Cloudy
+            return '🌥';
         }
         else{
             //None
@@ -31,17 +40,21 @@
     var oneword = data.query.results.channel.item.condition.text;
     var weathercode = data.query.results.channel.item.condition.code;
     var temp = Math.round((data.query.results.channel.item.condition.temp - 32) * 5 / 9);
-    window.onload = function () {
-    	document.getElementById('onewordweather').innerHTML = oneword;
+      window.addEventListener('load', function (){
+        document.getElementById('onewordweather').innerHTML = oneword;
         document.getElementById('emoji-left').innerHTML = '<h2>'+code2Emoji(weathercode)+'</h2>';
-    	document.getElementById('temp').innerHTML = temp + ' °C';
+        document.getElementById('temp').innerHTML = temp + ' °C';
 
         //Hidden div
         
         var forecast = data.query.results.channel.item.forecast;
         for (var i = 0; i < 5; i++) {
+             // console.log(forecast[i].code+'means '+forecast[i].text)
             var high = Math.round((forecast[i].high - 32) * 5 / 9);
             $("#wet-list").append("<li><h2 style='font-weight: 200; margin: 0;' ><span style='margin: 0; font-size: 25pt;'>"+code2Emoji(forecast[i].code)+"</span> "+forecast[i].date+", "+forecast[i].text+"</h2></li>");
         }    
-    }
+    });
   };
+
+
+
